6853aa943cd4d261a230f4be45261ba2903370cd
GOUFANG2021 <82783366+GOUFANG2021@users.noreply.github.com>
2025-03-20 16:03:26 +1300

    Update BRI_Calcium_Tartrate_Predictor_app.py

--- BRI_Calcium_Tartrate_Predictor_app.py
+++ BRI_Calcium_Tartrate_Predictor_app.py
@@ -67,8 +67,7 @@ if "simulation_count" not in st.session_state:
 
 with col1:
     # STEP 1: DOWNLOAD TEMPLATE
-     st.subheader("Step 1: Download the data format and enter your wine information in the input sheet.", unsafe_allow_html=True)
-    
+     st.subheader("Step 1: Download the data format and enter your wine information in the input sheet.")  
 
     template_path = "Wine Data.xlsx"
     download_result_template = download_from_github(DATA_TEMPLATE_URL, template_path)
